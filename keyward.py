e23b8b1a6ae19254b7ec966f6f2b3e2a5ae36e1f
o00o98 <o00o98@inha.edu>
2024-01-15 10:58:16 +0900

    v0.1

---
+++ keyward.py
@@ -0,0 +1,14 @@
+help("keywords")
+
+abc = 7
+Abc = 8
+# abc Abc는 서로 다른 변수임
+print(abc, Abc)
+
+test9 = 77
+# 9test: 변수의 이름은 숫자로 시작할 수 없음 / 띄어쓰기에 유의해야 함
+_9tes = 66
+print(test9,_9tes)
+
+# False : 예약어이기 때문에 변수의 이름으로 사용할 수 없음
+
